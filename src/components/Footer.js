335e753b1d398a37ffd9ba9a3d8db46405728c62
vhdttdhv <72727639+vhdttdhv@users.noreply.github.com>
2021-11-26 15:49:50 -0800

    First upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,24 @@ import "../styles/Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer-mailing">
-        <div className="footer-mailing-address">Updates in your inbox...</div>
-        <div className="footer-submit">GO</div>
+      <div className="footer-info">
+        <div className="footer-mailing">
+          <div className="footer-mailing-address">Updates in your inbox...</div>
+          <div className="footer-submit">GO</div>
+        </div>
+        <div className="footer-nav">
+          <a href="/">Home</a>
+          <a href="/">Careers</a>
+          <a href="/">Pricing</a>
+          <a href="/">Community</a>
+          <a href="/">Products</a>
+          <a href="/">About Us</a>
+          <a href="/">Privacy Policy</a>
+        </div>
+        <div className="footer-img">
+          <img src="/images/logo.svg" alt="logo" />
+        </div>
       </div>
-      <div className="footer-nav">
-        <a href="/">Home</a>
-        <a href="/">Careers</a>
-        <a href="/">Pricing</a>
-        <a href="/">Community</a>
-        <a href="/">Products</a>
-        <a href="/">Privacy Policy</a>
-        <a href="/">About Us</a>
-      </div>
-      <img src="/images/logo.svg" alt="logo" />
 
       <footer>Copyright 2021. All right reserved</footer>
     </div>
